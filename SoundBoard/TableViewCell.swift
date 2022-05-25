bd9d20053da5766fb63dde76acf5f8fcaa9c4a61
EstefaniDavila <76929037+EstefaniDavila@users.noreply.github.com>
2022-05-25 13:17:19 -0500

    Update TableViewCell.swift

--- SoundBoard/TableViewCell.swift
+++ SoundBoard/TableViewCell.swift
@@ -1,11 +1,3 @@
-//
-//  TableViewCell.swift
-//  SoundBoard
-//
-//  Created by Mac 17 on 5/25/21.
-//  Copyright © 2021 deah. All rights reserved.
-//
-
 import UIKit
 
 class TableViewCell: UITableViewCell {
